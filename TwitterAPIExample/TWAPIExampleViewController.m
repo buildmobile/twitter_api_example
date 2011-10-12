39543b29cdb4088c33c241e8dbc4d1aa8e0175ba
Andy <arcwhite@arcwhite.org>
2011-10-13 00:38:55 +1100

    Added out getTweet function, twiddled the storyboard a bit, and adjusted our viewDidLoad message

--- TwitterAPIExample/TWAPIExampleViewController.m
+++ TwitterAPIExample/TWAPIExampleViewController.m
@@ -23,8 +23,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.tweetLabel = (UILabel*)[self.view viewWithTag:1];
+    
+    [self getTweet];
 }
+
 
 - (void)viewDidUnload
 {
@@ -61,6 +65,45 @@
     } else {
         return YES;
     }
+}
+
+#pragma mark - Twitter Interactions
+
+-(void)getTweet
+{
+    // Specify the URL and parameters
+    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1/statuses/public_timeline.json"];   
+    NSDictionary *parameters = [NSDictionary dictionaryWithObjectsAndKeys:@"1", @"count", nil];
+    
+    // Create the TweetRequest object
+    TWRequest *tweetRequest = [[TWRequest alloc] initWithURL:url parameters:parameters requestMethod:TWRequestMethodGET];    
+    
+    [tweetRequest performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+        // Request completed and we have data
+        // Output it!
+        
+        NSError *jsonError = nil;
+        id timelineData = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableContainers error:&jsonError];
+        
+        if(timelineData == NULL) {
+            // There was an error changing the data to a Foundation Object,
+            // so we'll output a bunch of debug information.
+            NSString *myString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+            NSLog(@"\n\nConversion to object failed.");
+            NSLog(@"HTTP Response code: %d", [urlResponse statusCode]);
+            NSLog(@"Output from server: %@", myString);
+            NSLog(@"JSON Error: %@\n\n", [jsonError localizedDescription]);
+            abort();
+            // TODO: Show a graceful error message here
+        }
+        
+        NSDictionary *timelineDict = (NSDictionary*) timelineData;
+        NSLog(@"Foo!");
+        NSLog(@"%@", timelineDict);
+        
+        self.tweetLabel.text = [[(NSArray*)timelineDict objectAtIndex:0] objectForKey:@"text"];
+
+    }];
 }
 
 @end
