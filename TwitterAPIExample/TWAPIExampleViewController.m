206bbcd87712d1747d9e144fb3971ce96e04488c
Andy <arcwhite@arcwhite.org>
2011-12-04 18:16:07 +1100

    Modified getTweet method to use the AccountStore.

--- TwitterAPIExample/TWAPIExampleViewController.m
+++ TwitterAPIExample/TWAPIExampleViewController.m
@@ -84,12 +84,29 @@
 
 -(void)getTweet
 {
-    // Specify the URL and parameters
-    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1/statuses/public_timeline.json"];   
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1/statuses/home_timeline.json"];   
     NSDictionary *parameters = [NSDictionary dictionaryWithObjectsAndKeys:@"1", @"count", nil];
     
-    // Create the TweetRequest object
     TWRequest *tweetRequest = [[TWRequest alloc] initWithURL:url parameters:parameters requestMethod:TWRequestMethodGET];    
+    
+    ACAccountType *twitterAccountType = [self.store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    NSArray *accounts = [self.store accountsWithAccountType:twitterAccountType];
+    
+    if([accounts count] == 0) {
+        // We need to get the user to add an account!
+        // This is pretty cheap, but we'll zap them out to the Settings app, where they can add an account.
+        // When they return, we'll re-run viewDidLoad to get access to the account.
+        // TODO: Show a nice UI to your users to add/select which account they wish to use.
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=TWITTER"]];
+        [self viewDidLoad];
+        return;
+    }
+    
+    ACAccount *account = [accounts objectAtIndex:0];
+    //TODO: We should let the user choose which account they're using
+    
+    tweetRequest.account = account;
     
     [tweetRequest performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
         // Request completed and we have data
@@ -99,23 +116,18 @@
         id timelineData = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableContainers error:&jsonError];
         
         if(timelineData == NULL) {
-            // There was an error changing the data to a Foundation Object,
-            // so we'll output a bunch of debug information.
             NSString *myString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
-            NSLog(@"\n\nConversion to object failed.");
-            NSLog(@"HTTP Response code: %d", [urlResponse statusCode]);
-            NSLog(@"Output from server: %@", myString);
-            NSLog(@"JSON Error: %@\n\n", [jsonError localizedDescription]);
+            NSLog(@"Conversion to object failed.");
+            NSLog(@"%d", [urlResponse statusCode]);
+            NSLog(@"%@", myString);
+            NSLog(@"%@", [jsonError localizedDescription]);
             abort();
             // TODO: Show a graceful error message here
         }
         
         NSDictionary *timelineDict = (NSDictionary*) timelineData;
-        NSLog(@"Foo!");
         NSLog(@"%@", timelineDict);
-        
         self.tweetLabel.text = [[(NSArray*)timelineDict objectAtIndex:0] objectForKey:@"text"];
-
     }];
 }
 
